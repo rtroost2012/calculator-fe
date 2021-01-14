3f89568ea9f90dff4c9772e1ed58f241d261f423
rtroost <r.troost@quintor.nl>
2021-01-14 10:46:16 +0100

    added component test for calculator

--- src/app/component/calculator/calculator.component.spec.ts
+++ src/app/component/calculator/calculator.component.spec.ts
@@ -1,12 +1,18 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { CalculationResult } from 'src/app/model/calculation-result';
+import { CalculatorResultsService } from 'src/app/service/calculator-results.service';
+import { CalculatorService } from 'src/app/service/calculator.service';
 
 import { CalculatorComponent } from './calculator.component';
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
   let fixture: ComponentFixture<CalculatorComponent>;
+  let mockCalculatorResultsService: CalculatorResultsService;
+  let mockCalculatorService: CalculatorService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,12 +23,53 @@ describe('CalculatorComponent', () => {
   });
 
   beforeEach(() => {
+    // Component fixture
     fixture = TestBed.createComponent(CalculatorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    // Mock services
+    mockCalculatorResultsService = TestBed.inject(CalculatorResultsService);
+    mockCalculatorService = TestBed.inject(CalculatorService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load previous results', () => {
+    // given
+    let testdata: Array<CalculationResult> = [{
+      id: 1, a: 10.0, b: 20.0, type: 'ADDITION', result: 30.0
+    }];
+    
+    // when
+    spyOn(mockCalculatorResultsService, 'findAll').and.returnValue(of(testdata));
+    component.loadPrevious();
+
+    // then
+    expect(mockCalculatorResultsService.findAll).toHaveBeenCalledTimes(1);
+    expect(component.results).toEqual(testdata);
+  });
+
+  it('should be able to calculate', () => {
+    // given
+    let id = 1;
+    let first = 100.0;
+    let second = 200.0;
+    let type = 1; // subtraction
+    let testdata: CalculationResult = { id: id, a: first, b: second, type: 'ADDITION', result: -100.0 };
+
+    component.first = first;
+    component.second = second;
+    component.type = type;
+
+    // when
+    spyOn(mockCalculatorService, 'byType').and.returnValue(of(testdata));
+    component.calculate();
+
+    // then
+    expect(mockCalculatorService.byType).toHaveBeenCalledWith(first, second, type);
+    expect(component.results).toEqual([testdata]);
   });
 });
--- src/app/component/calculator/calculator.component.ts
+++ src/app/component/calculator/calculator.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CalculationResult } from 'src/app/model/calculation-result';
+import { CalculatorResultsService } from 'src/app/service/calculator-results.service';
 import { CalculatorService } from 'src/app/service/calculator.service';
 
 @Component({
@@ -11,21 +13,36 @@ export class CalculatorComponent implements OnInit {
   first: Number = 0;
   second: Number = 0;
   type: Number = 0; // ADD,SUBTRACT,MULTIPLY,DIVIDE (0-3)
-  results = [];
+  results: Array<CalculationResult> = []; // previous results
 
-  constructor(private calculatorService: CalculatorService) { }
+  constructor(
+    private calculatorService: CalculatorService,
+    private calculatorResultsService: CalculatorResultsService
+  ) { }
 
   ngOnInit(): void {
+    this.loadPrevious();
   }
 
-  calculate(): void {
-    let result: Number = 0.0;
+  /**
+   * Attempts to fetch to fetch the previous calculations and
+   * load them into the page
+   */
+  loadPrevious(): void {
+    this.calculatorResultsService.findAll().subscribe(
+      (res: Array<CalculationResult>) => this.results = this.results.concat(res),
+      (err: String) => alert('Error occurred while attempting to fetch previous calculations')
+    );
+  }
 
+  /**
+   * Attempts to calculate and store the given inputs by calling the
+   * appropriate API endpoint. 
+   */
+  calculate(): void {
     this.calculatorService.byType(this.first, this.second, this.type).subscribe(
-      res => { 
-        this.results.push(res);
-        console.log(this.results);
-      },
-      err => alert('Error occurred while attempting to calculate'));
+      (res: CalculationResult) => this.results.push(res),
+      (err: String) => alert('Error occurred while attempting to calculate')
+    );
   }
 }
